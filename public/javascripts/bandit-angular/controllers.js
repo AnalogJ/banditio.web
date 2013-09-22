14bc80a55a529ceb1d37c5bc2c7360c589ad2bbf
Jason Kulatunga <jk17@ualberta.ca>
2013-09-22 10:51:39 -0700

    working initial design.

--- public/javascripts/bandit-angular/controllers.js
+++ public/javascripts/bandit-angular/controllers.js
@@ -2,12 +2,49 @@ angular.module('banditApp.controllers', [ 'banditApp.services','btford.socket-io
     //Panel Controllers
     .controller('roomCtrl', function ($scope, socket) {
         console.log('im in the room ctrl');
-        socket.on('socket:message', function (data) {
-            console.log('MESSAGE FROM INSIDE CONTROLLER: '+ data)
-        });
+        $scope.resources = {};
         socket.forward('message', $scope);
         $scope.$on('socket:message', function (ev, data) {
             //$scope.theData = data;
+            parseMessage(data)
+
+
+
             console.log('MESSAGE FROM INSIDE CONTROLLER: '+ data)
         });
+
+        function parseMessage(message){
+            console.log(message)
+            if(message.resource_id){
+                //initialize if empty
+                $scope.resources[message.resource_id] = $scope.resources[message.resource_id] || {};
+            }
+            switch (message.message_type) {
+                case "REQUEST_HEADER":
+                    $scope.resources[message.resource_id].request_header = message.payload;
+                    break;
+                case "REQUEST_BODY":
+                    break;
+                case "RESPONSE_HEADER":
+                    $scope.resources[message.resource_id].response_header = message.payload.response_header;
+                    $scope.resources[message.resource_id].response_info = message.payload.response_info;
+                    break;
+                case "RESPONSE_BODY":
+                    $scope.resources[message.resource_id].response_body = message.payload;
+                    break;
+                case "RES_CONSOLE_MESSAGE":
+                    break;
+                case "RES_CONSOLE_COMMAND":
+                    break;
+                case "RES_DISCONNECTED":
+                    break;
+                default:
+                    break;
+            }
+
+
+
+        }
+
+
     })
